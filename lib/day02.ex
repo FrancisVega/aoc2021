11ba354c40fb0feb1f96954a1af21d6d1dcb096d
FrancisVega <hisconer@gmail.com>
2021-12-03 22:01:06 +0100

    refactor

--- lib/day02.ex
+++ lib/day02.ex
@@ -17,32 +17,22 @@ defmodule Day02 do
   defp down(x), do: instruction(x, "down")
   defp up(x), do: instruction(x, "up")
 
-  defp calculate_values(x) do
-    x
+  defp values_from_command(list, command) do
+    list
+    |> Enum.filter(command)
     |> Enum.map(&value/1)
     |> Enum.map(&String.to_integer/1)
     |> Enum.sum()
   end
 
   def solution1 do
-    instructions =
+    input =
       AOCUtils.readInputFile(@path)
-      |> Enum.map(fn x -> String.split(x) end)
+      |> Enum.map(&String.split/1)
 
-    forward =
-      instructions
-      |> Enum.filter(&forward/1)
-      |> calculate_values()
-
-    down =
-      instructions
-      |> Enum.filter(&down/1)
-      |> calculate_values()
-
-    up =
-      instructions
-      |> Enum.filter(&up/1)
-      |> calculate_values()
+    forward = values_from_command(input, &forward/1)
+    down = values_from_command(input, &down/1)
+    up = values_from_command(input, &up/1)
 
     forward * (down - up)
   end
